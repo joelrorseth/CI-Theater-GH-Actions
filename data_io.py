0d7b16d8a0935af18dc7223fbc95525c91eabb60
Joel Rorseth <joelrorseth@gmail.com>
2022-03-06 11:58:05 -0500

    Add function to write yaml

--- data_io.py
+++ data_io.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 from typing import Any, Union
 
 OutputFile = Union[str, None]
@@ -8,6 +9,12 @@ def write_res_json(res_json: Any, output_filename: OutputFile = None) -> None:
     if output_filename is not None:
         with open(output_filename, 'w') as f:
             json.dump(res_json, f)
+
+
+def write_res_yaml(res_yaml: str, output_filename: OutputFile = None) -> None:
+    if output_filename is not None:
+        with open(output_filename, 'w') as outfile:
+            yaml.dump(res_yaml, outfile)
 
 
 def write_df_csv(df, output_filename: OutputFile = None, header: bool = False, index: bool = False):
