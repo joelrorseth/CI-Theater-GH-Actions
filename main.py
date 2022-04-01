5e40ed04a75f0e746943ce65dec60108702ac9fc
Joel Rorseth <joelrorseth@gmail.com>
2022-04-01 01:34:55 -0400

    Add initial project loading to main

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-if __name__ == "__main__":
+import math
+from filter_projects import get_initial_projects
+
+MEMBER_COUNT_BINS = [
+    [2, 4],
+    [5, 8],
+    [9, 15],
+    [16, 24],
+    [25, math.inf]
+]
+
+DATA_FOLDER = 'data'
+PROJECTS_STAGE_0_PATH = f"{DATA_FOLDER}/projects_stage_0.csv"
+
+if __name__ == '__main__':
+    print('CI Theater (GitHub Actions edition)')
+    print(
+        f"NOTE: Please delete any stale data in ./{DATA_FOLDER}/ before running")
+    print('Starting the experiment...')
+    print()
+
+    print('[!] Beginning filtering phase:')
+    get_initial_projects(PROJECTS_STAGE_0_PATH)
+
     # TODO: Sequence all experiment operations
     pass
