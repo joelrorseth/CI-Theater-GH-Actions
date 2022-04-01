038fc0ccb68a0527f80539cca5466f763106ce13
joelrorseth <joelrorseth@gmail.com>
2022-03-31 22:37:40 -0400

    Render project membership count histogram

--- analyze.py
+++ analyze.py
@@ -16,6 +16,8 @@ def analyze_project_member_count(project_membership_count_dist_img_path: str):
 
     # Count the number of projects have each distinct membership count
     member_count_counts = repo_member_counts.value_counts()
+    print('Frequency of each member count:')
+    print(member_count_counts)
     plot_value_counts_histogram(
         member_count_counts,
         project_membership_count_dist_img_path
@@ -55,8 +57,11 @@ if __name__ == '__main__':
     project_membership_count_dist_img_path = 'figures/project_membership_count_dist.png'
     coverage_boxplot_img_path = 'figures/coverage_boxplot.png'
 
+    """
+    # Produce a histogram illustrating the distribution of project member counts
+    analyze_project_member_count(project_membership_count_dist_img_path)
+    """
+
     # Produce a boxplot illustrating project test coverage by language
     analyze_coverage(project_coverage_path, coverage_boxplot_img_path)
 
-    # Produce a histogram illustrating the distribution of project member counts
-    analyze_project_member_count(project_membership_count_dist_img_path)
