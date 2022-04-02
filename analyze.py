cd4b6d69b760b8a796a5bba4e6e6bd25a5b77470
Joel Rorseth <joelrorseth@gmail.com>
2022-04-02 11:37:48 -0400

    Add coverage and membership analysis to main

--- analyze.py
+++ analyze.py
@@ -1,5 +1,5 @@
 from coverage import load_coverage
-from data_io import read_dict_from_json_file, write_series_to_json_file
+from data_io import write_series_to_json_file
 from plot import plot_code_coverage_boxplots, plot_project_member_counts_histogram
 from projects import load_project_members
 
@@ -16,10 +16,8 @@ def analyze_project_member_count(project_membership_count_dist_img_path: str,
     print(f"Members per project median: {repo_member_counts.median()}")
     print(f"Members per project std dev: {repo_member_counts.std()}")
 
-    # Count the number of projects have each distinct membership count
+    # Count the number of projects having each distinct membership count
     member_count_counts = repo_member_counts.value_counts()
-    print('Frequency of each member count:')
-    print(member_count_counts)
     write_series_to_json_file(
         member_count_counts, project_membership_count_dist_path)
     plot_project_member_counts_histogram(
@@ -27,46 +25,15 @@ def analyze_project_member_count(project_membership_count_dist_img_path: str,
         project_membership_count_dist_img_path
     )
 
-    """
-    repos_gte1 = repo_member_counts[repo_member_counts >= 1].shape[0]
-    repos_gte2 = repo_member_counts[repo_member_counts >= 2].shape[0]
-    repos_gte5 = repo_member_counts[repo_member_counts >= 5].shape[0]
-    repos_gte10 = repo_member_counts[repo_member_counts >= 10].shape[0]
-    repos_gte25 = repo_member_counts[repo_member_counts >= 25].shape[0]
-
-    print(f"{repos_gte1} repos have >= 1 member")
-    print(f"{repos_gte2} repos have >= 2 members")
-    print(f"{repos_gte5} repos have >= 5 members")
-    print(f"{repos_gte10} repos have >= 10 members")
-    print(f"{repos_gte25} repos have >= 25 members")
-    """
-
     print("[!] Done analyzing project member counts")
 
 
-def analyze_coverage(project_coverage_path: str, coverage_boxplot_img_path: str) -> None:
+def analyze_coverage(coverage_path: str, coverage_boxplot_img_path: str) -> None:
     """
     RQ2: How common is running a build in a software project with poor test coverage?
     To answer this RQ, we produce a figure containing a boxplot for each programming language,
     where each boxplot illustrates the distribution of code coverage for projects using this
     language.
     """
-    coverage_by_lang = load_coverage(project_coverage_path)
+    coverage_by_lang = load_coverage(coverage_path)
     plot_code_coverage_boxplots(coverage_by_lang, coverage_boxplot_img_path)
-
-
-if __name__ == '__main__':
-    project_coverage_path = 'data/project_coverage.json'
-    project_membership_count_dist_path = 'results/project_membership_count_dist.json'
-
-    project_membership_count_dist_img_path = 'figures/project_membership_count_dist.png'
-    coverage_boxplot_img_path = 'figures/coverage_boxplot.png'
-
-    """
-    # Produce a histogram illustrating the distribution of project member counts
-    analyze_project_member_count(
-        project_membership_count_dist_img_path, project_membership_count_dist_path)
-    """
-
-    # Produce a boxplot illustrating project test coverage by language
-    analyze_coverage(project_coverage_path, coverage_boxplot_img_path)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from analyze import analyze_coverage, analyze_project_member_count
 from augment import get_coveralls_info, get_default_branches_for_projects, get_workflow_runs
-from config import DATA_FOLDER, SUPPORTED_LANGUAGES
+from config import DATA_FOLDER, RESULTS_FOLDER, SUPPORTED_LANGUAGES
 from filter_projects import (
     filter_by_default_branch_existence,
     filter_by_using_ci,
@@ -10,23 +11,31 @@ from filter_projects import (
     get_initial_projects
 )
 
+# These filenamess / paths are declared in order of creation
 PROJECTS_STAGE_0_PATH = f"{DATA_FOLDER}/projects_stage_0.csv"
 PROJECTS_STAGE_1_PATH = f"{DATA_FOLDER}/projects_stage_1.csv"
 PROJECTS_STAGE_2_PATH = f"{DATA_FOLDER}/projects_stage_2.csv"
 PROJECTS_STAGE_3_PATH = f"{DATA_FOLDER}/projects_stage_3.csv"
-PROJECTS_STAGE_4_PATH = f"{DATA_FOLDER}/projects_stage_4.csv"
-PROJECTS_STAGE_5_PATH = f"{DATA_FOLDER}/projects_stage_5.csv"
-PROJECTS_STAGE_6_PATH = f"{DATA_FOLDER}/projects_stage_6.csv"
 WORKFLOWS_STAGE_3_PREFIX = f"{DATA_FOLDER}/workflows_stage_3"
-WORKFLOW_YAML_STAGE_4_PREFIX = f"{DATA_FOLDER}/workflow_yaml_stage_3"
 WORKFLOWS_STAGE_3_PATH = f"{WORKFLOWS_STAGE_3_PREFIX}.json"
+PROJECTS_STAGE_4_PATH = f"{DATA_FOLDER}/projects_stage_4.csv"
 WORKFLOWS_STAGE_4_PATH = f"{DATA_FOLDER}/workflows_stage_4.json"
-WORKFLOWS_STAGE_6_PATH = f"{DATA_FOLDER}/workflows_stage_6.json"
+WORKFLOW_YAML_STAGE_4_PREFIX = f"{DATA_FOLDER}/workflow_yaml_stage_3"
 DEFAULT_BRANCHES_PREFIX = f"{DATA_FOLDER}/default_branches"
 DEFAULT_BRANCHES_PATH = f"{DEFAULT_BRANCHES_PREFIX}.json"
+PROJECTS_STAGE_5_PATH = f"{DATA_FOLDER}/projects_stage_5.csv"
 WORKFLOW_RUNS_PREFIX = f"{DATA_FOLDER}/workflow_runs"
+PROJECTS_STAGE_6_PATH = f"{DATA_FOLDER}/projects_stage_6.csv"
+WORKFLOWS_STAGE_6_PATH = f"{DATA_FOLDER}/workflows_stage_6.json"
 PROJECT_COVERAGE_PREFIX = f"{DATA_FOLDER}/project_coverage"
 LANGUAGE_COVERAGE_PATH = f"{DATA_FOLDER}/language_coverage.json"
+
+ORIGINAL_PROJECTS_MEMBER_DIST_IMG_PATH = f"{RESULTS_FOLDER}/original_projects_member_dist.png"
+ORIGINAL_PROJECTS_MEMBER_DIST_JSON_PATH = f"{RESULTS_FOLDER}/original_projects_member_dist.json"
+FINAL_PROJECTS_MEMBER_DIST_IMG_PATH = f"{RESULTS_FOLDER}/final_projects_member_dist.png"
+FINAL_PROJECTS_MEMBER_DIST_JSON_PATH = f"{RESULTS_FOLDER}/final_projects_member_dist.json"
+PROJECT_COVERAGE_BY_LANG_IMG_PATH = f"{RESULTS_FOLDER}/project_coverage_by_language.png"
+
 
 if __name__ == '__main__':
     print('CI Theater (GitHub Actions edition)')
@@ -58,5 +67,16 @@ if __name__ == '__main__':
                                    WORKFLOW_RUNS_PREFIX)
     get_coveralls_info(PROJECTS_STAGE_6_PATH, WORKFLOWS_STAGE_6_PATH, DEFAULT_BRANCHES_PATH,
                        WORKFLOW_RUNS_PREFIX, PROJECT_COVERAGE_PREFIX, LANGUAGE_COVERAGE_PATH)
+
+    print('[!] Beginning analysis phase')
+    analyze_project_member_count(
+        ORIGINAL_PROJECTS_MEMBER_DIST_IMG_PATH,
+        ORIGINAL_PROJECTS_MEMBER_DIST_JSON_PATH
+    )
+    analyze_project_member_count(
+        FINAL_PROJECTS_MEMBER_DIST_IMG_PATH,
+        FINAL_PROJECTS_MEMBER_DIST_JSON_PATH
+    )
+    analyze_coverage(LANGUAGE_COVERAGE_PATH, PROJECT_COVERAGE_BY_LANG_IMG_PATH)
 
     print('Done')
